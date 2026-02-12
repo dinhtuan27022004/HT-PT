884e16216e3c126d12bb0dd4de9186aedadaa46e
Tuấn Nguyễn Đình <dinhtuan27022004@gmail.com>
2026-02-12 15:59:19 +0700

    update

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { all } from 'axios'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true,
+    port: 5173,
+    allowedHosts: 'all'
+  }
 })
