103b8069c52d050b5086478e27ba1ea390020404
Tuấn Nguyễn Đình <dinhtuan27022004@gmail.com>
2026-02-12 11:36:20 +0700

    update

---
+++ frontend/src/components/layout/Navbar.jsx
@@ -0,0 +1,14 @@
+function Navbar() {
+    return (
+        <nav className="navbar glass-card">
+            <div className="logo gradient-text">E-Web Project</div>
+            <div className="nav-links">
+                <a href="#">Home</a>
+                <a href="#">Shop</a>
+                <a href="#">Contact</a>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar
